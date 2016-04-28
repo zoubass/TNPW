ff94d3866c47cf0e6868ebf6e8d6c84c044a2b00
zoubass <zoubas@seznam.cz>
2016-04-28 22:33:32 +0200

    Connected to database, added login page, error page, admin restricted sites

--- eshop/src/main/java/neo/controller/WebController.java
+++ eshop/src/main/java/neo/controller/WebController.java
@@ -1,14 +1,30 @@
 package neo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller()
 public class WebController {
 
-    @RequestMapping(value = ("/index"), method = RequestMethod.GET)
-    public String helloWorld() {
-        return "index";
+    @RequestMapping("/admin")
+    public String showAdminPage(Model model) {
+        return "admin";
+    }
+
+
+    @RequestMapping("/error")
+    public String error(HttpServletRequest request, Model model) {
+        model.addAttribute("errorCode", request.getAttribute("javax.servlet.error.status_code"));
+        Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+        String errorMessage = null;
+        if (throwable != null) {
+            errorMessage = throwable.getMessage();
+        }
+        model.addAttribute("errorMessage", errorMessage);
+        return "error";
     }
 }
--- eshop/src/test/java/neo/DaoTest.java
+++ eshop/src/test/java/neo/DaoTest.java
@@ -1,22 +1,24 @@
 package neo;
 
 import neo.dao.UserDao;
+import neo.dao.impl.UserDaoImpl;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * Created by Admin on 12.4.2016.
  */
+
+@ContextConfiguration("classpath:META-INF/spring/application-context.xml")
 public class DaoTest {
 
-//    @Autowired
-//    private UserDao;
+    @Autowired
+    private UserDao dao;
 
     @Test
     public void testConnection() {
-
-
-        Assert.assertTrue(true);
+        Assert.assertNotNull(dao);
     }
 }
