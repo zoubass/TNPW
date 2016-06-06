d2fb7bb24cf2a14b73218d57a3ad6076e23f7954
zoubass <zoubas@seznam.cz>
2016-06-07 01:05:42 +0200

    Implemented User and Product repositories and models.

---
+++ eshop/src/main/java/cz/tnpwteam/dao/CartRepository.java
@@ -0,0 +1,10 @@
+package cz.tnpwteam.dao;
+
+import cz.tnpwteam.model.Cart;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Admin on 7.6.2016.
+ */
+public interface CartRepository /*extends CrudRepository<Cart,Long>*/ {
+}
